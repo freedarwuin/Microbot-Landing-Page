3592afb891af4dd85140de2a880c345cb027dad3
chsami <sami.chkhachkhi@gmail.com>
2024-11-03 23:00:49 +0100

    release pages added

---
+++ microbot/releases/release.js
@@ -0,0 +1,9 @@
+const releases = [
+    {
+        date: new Date('11/03/2024'), //MM/dd/YYYY
+        version: '1.5.9',
+        title: '1.5.9 Release',
+        content: 'Web walker Teleports, Blue Dragons Plugin, New Agility courses, New Herb Run Plugin, New Mahogany homes plugin...',
+        author: 'Mocrosoft'
+    }
+]
