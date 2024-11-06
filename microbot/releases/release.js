3228b892cb17b7906d1123afe5ab9565379484e7
chsami <sami.chkhachkhi@gmail.com>
2024-11-06 16:49:12 +0100

    bump 1.6.0

--- microbot/releases/release.js
+++ microbot/releases/release.js
@@ -1,16 +1,16 @@
 const releases = [
     {
-        date: new Date('11/03/2024'), //MM/dd/YYYY
-        version: '1.5.9',
-        title: '1.5.9 Release',
-        content: 'Web walker Teleports, Blue Dragons Plugin, New Agility courses, New Herb Run Plugin, New Mahogany homes plugin...',
-        author: 'Mocrosoft'
-    },
-    {
         date: new Date('11/06/2024'), //MM/dd/YYYY
         version: '1.6.0',
         title: '1.6.0 Release',
         content: 'Runelite Update',
+        author: 'Mocrosoft'
+    },
+    {
+        date: new Date('11/03/2024'), //MM/dd/YYYY
+        version: '1.5.9',
+        title: '1.5.9 Release',
+        content: 'Web walker Teleports, Blue Dragons Plugin, New Agility courses, New Herb Run Plugin, New Mahogany homes plugin...',
         author: 'Mocrosoft'
     }
 ]
